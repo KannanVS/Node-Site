77da1398412eeb8d287377998405da673a7e8ecc
KannanVS <kannansanthanam97@gmail.com>
2020-08-12 04:52:30 +0200

    Adding product page

--- index.js
+++ index.js
@@ -5,11 +5,22 @@ app.use(express.static('public'));
 app.set('view engine', 'ejs')
 
 app.get('/', (req, res) => {
-    res.render("home")
+    res.render("home", {
+        title: "Home"
+    });
 })
 
 app.get('/about', (req, res) => {
-    res.render("about")
+    res.render("about", {
+        title: "About US"
+    });
 })
+
+app.get('/product', (req, res) => {
+    res.render("product", {
+        title: "Women's Collection"
+    });
+})
+
 
 app.listen(3000);
